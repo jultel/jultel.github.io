fc51ccaf2b5143eb3015a33db63ad610bd32bcc4
Yuliia <telishchuk25265@gmail.com>
2018-07-09 23:50:55 +0300

    adfsg

--- js/helper.js
+++ js/helper.js
@@ -1,21 +1,50 @@
-function fillplaceholder(){
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(json =>{
-        console.log(json);
-        document.getElementById("placeholder1").innerHTML=json.body;
+// function fillplaceholder(){
+//     fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     .then(response => response.json())
+//     .then(json =>{
+//         console.log(json);
+//         document.getElementById("placeholder1").innerHTML=json.body;
 
-    })
+//     })
        
 
-}
-fillplaceholder();
-var object1js={
-"name":"Telishchuk",
-"city":"lviv",
-"eage":21,
-"feemale": true,
-"xcord":49.795149, "y":24.065178
-};
-document.getElementById("placeholder2").innerHTML=object1js.xcord;
+// }
+// fillplaceholder();
+// var object1js={
+// "name":"Telishchuk","y":24.065178
+// };
+// document.getElementById("placeholder2").innerHTML=object1js.xcord;
 
+function sendForm(e){
+    e.preventDefault();
+    fetch('form', {
+    headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            name: document.querySelector('#nameText').value,
+            email: document.querySelector('#e-mailText').value,
+            message: document.querySelector('#textText').value
+        })
+});
+
+console.log(document.querySelector('#nameText').value);
+
+}
+
+document.querySelector('#SendBtn').addEventListener('click', sendForm);
+
+
+function getJoke(e){
+e.preventDefault();
+fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json'
+            },
+            method: 'GET'
+}).then((res)=> {return res.json();})
+.then((json)=>{ 
+   document.querySelector('#joke').innerHTML=json.joke;});
+}
+document.querySelector('#jokeBtn').addEventListener('click',getJoke);
